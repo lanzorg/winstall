245c492bc13ec2c4a356b95c02964842a751f2ea
anonymous <anonymous@example.com>
2020-09-06 11:19:40 +0200

    Add a new package

--- winstall/packages/__init__.py
+++ winstall/packages/__init__.py
@@ -1,4 +1,5 @@
 from packages.chromium import Chromium
 from packages.figma import Figma
+from packages.imposition_wizard import ImpositionWizard
 from packages.nodejs import Nodejs
 from packages.yarn import Yarn
---
+++ winstall/packages/imposition_wizard.py
@@ -0,0 +1,47 @@
+import glob
+import os
+import re
+import shutil
+import subprocess
+from functools import cached_property
+
+import requests
+
+from packages.package import Package
+from utilities.downloaders import from_filecr
+from utilities.unarchivers import extract_dir
+from utilities.wincommons import get_version, purge_desktop_links
+
+
+class ImpositionWizard(Package):
+    @cached_property
+    def install_dir(self) -> str:
+        return os.path.join(os.environ.get("PROGRAMFILES"), "Appsforlife/Imposition Wizard 3")
+
+    @cached_property
+    def actual_version(self) -> str:
+        version = get_version(os.path.join(self.install_dir, "ImpositionWizard.exe"))
+        return version
+
+    @cached_property
+    def latest_version(self) -> str:
+        address = "https://filecr.com/windows/imposition-wizard/"
+        content = requests.get(address).text
+        version = re.search("<h2>Imposition Wizard (.*)</h2>", content).group(1)
+        return version
+
+    async def download(self) -> str:
+        archive = await from_filecr("https://filecr.com/windows/imposition-wizard/")
+        return archive
+
+    async def install(self) -> None:
+        if not self.is_updated:
+            archive = await self.download()
+            destination = extract_dir(archive, password="123")
+            program = glob.glob(f"{destination}/*.exe")[0]
+            command = f'"{program}" /S'
+            subprocess.run(command)
+            source = os.path.join(destination, "crack/ImpositionWizard.exe")
+            target = os.path.join(self.install_dir, "ImpositionWizard.exe")
+            shutil.copy(source, target)
+            purge_desktop_links("Imposition Wizard")
